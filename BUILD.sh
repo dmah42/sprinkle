eef9ff597e8c794fd0c3a6305ab3283cc59e79f6
Dominic Hamon <dma+github@stripysock.com>
2022-05-21 11:02:01 +0100

    update build to generate grpc protos

--- BUILD.sh
+++ BUILD.sh
@@ -4,10 +4,10 @@ GH=github.com/dominichamon/swarm
 OUT=bin
 
 echo "building proto"
-protoc -Iproto/ ./proto/swarm.proto --go_out=plugins=grpc:proto
+protoc --go-grpc_out=./ --go-grpc_opt=paths=source_relative proto/swarm.proto
 go build $GH/proto
 
 for b in worker swarm ui; do
-  echo "building $b"
-  go build -o $OUT/$b $GH/$b
+  echo "building command $b"
+  go build -o $OUT/$b $GH/cmd/$b
 done
